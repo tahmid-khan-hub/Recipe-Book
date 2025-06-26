c90056bc0fd356ddbb6f785923b070e9c675cce7
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-06-26 18:41:58 +0600

    updated errorPage and allRecipe

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router';
+import Lottie from "lottie-react";
+import errorLottie from "../../assets/lotties/404.json"
 
 const ErrorPage = () => {
     return (
         <div className='flex flex-col items-center justify-center h-screen'>
-            <img src="https://i.ibb.co/HDrDhbr0/image.png" />
+            <div className="w-72 h-72">
+              <Lottie animationData={errorLottie} loop />
+            </div>
             <p className='text-center text-2xl my-4'>Oops! The page you're looking for doesn't exist</p>
             <Link className="block mx-auto w-fit" to={`/`}><button className='btn text-white bg-gradient-to-r from-green-400 to-green-500 hover:from-green-500 hover:to-green-600 w-[200px] shadow-xl shadow-green-300'>Go Back Home</button></Link>
         </div>
